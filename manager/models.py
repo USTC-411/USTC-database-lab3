793ca7294d5cd2625f1044b82c25fb3ea23b3e38
ShadowTinker <shadowtinkery@gmail.com>
2020-05-12 08:11:40 +0800

    Finish the basic models of our database. There might be lots of errors, which should be fixed in the future. The followings are what I've done 1. sign up for manger in main app's settings 2. create a campus model 3. create a major model 4. create a class_ model 5. create a person abstract  model     5.1 create a submodel teacher     5.2 create a submodel student 6. create a StatusChange abstract model     6.1 create a submodel MajorTransfer     6.2 create a submodel GradeTransfer 7. create a Lesson model     7.1 create a valid_lesson model for those are valid, which can be used to record some data for a valid lesson. This must have some problems 8. create a ManyToMany table LessonSelect, in which the lesson selection info stores

--- manager/models.py
+++ manager/models.py
@@ -2,5 +2,213 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+class Campus(models.Model):
+  id = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30)
+  address = models.CharField(max_length=30)
+  def __str__(self):
+      return self.name
+  
+
+class Major(models.Model):
+  id = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30)
+  address = models.CharField(max_length=30)
+  principal = models.CharField(max_length=30)
+  campus = models.ForeignKey('Campus', on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
+
+class Class_(models.Model):
+  id = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30)
+  date = models.DateField()
+  head_teacher = models.CharField(max_length=30)
+  grade = models.DateField()
+  major = models.ForeignKey('Major', on_delete=models.CASCADE)
+  def __str__(self):
+    return self.name
+
+class Person(models.Model):
+  # Enum value for id_type
+  IDCARD = 'IDCard'
+  PASSPORT = 'Passport'
+  ID_TYPE = (
+    (IDCARD, '身份证')
+    (PASSPORT, '护照')
+  )
+  # Enum value for sex
+  MALE = 'male'
+  FEMALE = 'female'
+  SEX_TYPE = (
+    (MALE, '男')
+    (FEMALE, '女')
+  )
+  # Fields for model
+  id = models.CharField(max_length=30, primary_key=True)
+  id_type = models.CharField(choices=ID_TYPE, default=IDCARD)
+  name = models.CharField(max_length=30)
+  sex = models.CharField(choices=SEX_TYPE)
+  birthday = models.CharField(max_length=30)
+  nationality = models.CharField(max_length=30)
+  family_address = models.CharField(max_length=30, blank=True)
+  family_postcode = models.CharField(max_length=30, blank=True)
+  family_telephone = models.CharField(max_length=30, blank=True)
+  entry_date = models.DateField()
+  email = models.CharField(max_length=30)
+  class Meta:
+    abstract = True
+
+class Teacher(Person):
+  # Enum value for job title
+  PROFESSOR = 'professor'
+  ASSOCIATE_PROFESSOR = 'associate_professor'
+  TITLE_TYPE = (
+    (PROFESSOR, '教授')
+    (ASSOCIATE_PROFESSOR, '副教授')
+  )
+  # Fields for model
+  teacher_id = models.CharField(max_length=30, unique=True)
+  major = models.ForeignKey('Major', on_delete=models.CASCADE)
+  title = models.CharField(choices=TITLE_TYPE)
+  def __str__(self):
+    return self.name
+
+class Student(Person):
+  student_id = models.CharField(max_length=30, unique=True)
+  class_ = models.ForeignKey('Class_', on_delete=models.CASCADE)
+  lesson_select = models.ManyToManyField(Lesson, related_name='lesson_select', through='LessonSelect')
+  def __str__(self):
+    return self.name
+
+class StatusChange(models.Model):
+  chenge_id = models.CharField(max_length=30, primary_key=True)
+  change_date = models.DateField()
+  original_class = models.OneToOneField(Class_,on_delete=models.CASCADE)
+  current_class = models.OneToOneField(Class_,on_delete=models.CASCADE)
+  class Meta:
+    abstract = True
+
+class MajorTransfer(StatusChange):
+  student = models.OneToOneField(
+    Student,
+    on_delete = models.CASCADE,
+    related_name = 'major_transfer'
+  )
+  has_transfered_communist_youth_league_relationship = models.BooleanField(default=False)
+
+class GradeTransfer(StatusChange):
+  # Enum value of degrade reason
+  SUSPENSION = 'suspension'
+  VOLUNTEER_TEACHING = 'volunteer_teaching'
+  DEGRADE_REASON = (
+    (SUSPENSION, '休学')
+    (VOLUNTEER_TEACHING, '支教')
+  )
+  # Fields of this model
+  student = models.OneToOneField(
+    Student,
+    on_delete = models.CASCADE,
+    related_name = 'grade_transfer'
+  )
+  degrade_reason = models.CharField(choices=DEGRADE_REASON)
+
+class Lesson(models.Model):
+  # Enum value of test type
+  TEST = 'test'
+  DEBATE = 'debate'
+  TEST_TYPE = (
+    (TEST, '考试')
+    (DEBATE, '答辩')
+  )
+  # Enum value of lesson status
+  VALID = 'valid'
+  INVALID = 'invalid'
+  LESSON_STATUS = (
+    (VALID, 'valid')
+    (INVALID, 'invalid')
+  )
+  # Fields of this model
+  id = models.CharField(max_length=30, primary_key=True)
+  name = models.CharField(max_length=30, unique=True)
+  major = models.ForeignKey('Major', on_delete=models.CASCADE)
+  test_type = models.CharField(choices=TEST_TYPE, default=TEST)
+  lesson_status = models.CharField(choices=LESSON_STATUS, default=INVALID)
+
+class ValidLesson(models.Model):
+  # Enum value of semester
+  SPRING = 'spring'
+  AUTUMN = 'autumn'
+  SEMESTER = (
+    (SPRING, '春季学期')
+    (AUTUMN, '秋季学期')
+  )
+  # Enum value of lesson's begin time
+  class BEGIN_TIME_TYPE(models.IntegerChoices):
+    MONDAY1 = 11
+    MONDAY2 = 12
+    MONDAY3 = 13
+    MONDAY4 = 14
+    MONDAY5 = 15
+    MONDAY6 = 16
+    MONDAY7 = 17
+    MONDAY8 = 18
+    MONDAY9 = 19
+    TUESDAY1 = 21
+    TUESDAY2 = 22
+    TUESDAY3 = 23
+    TUESDAY4 = 24
+    TUESDAY5 = 25
+    TUESDAY6 = 26
+    TUESDAY7 = 27
+    TUESDAY8 = 28
+    TUESDAY9 = 29
+    WEDNESDAY1 = 31
+    WEDNESDAY2 = 32
+    WEDNESDAY3 = 33
+    WEDNESDAY4 = 34
+    WEDNESDAY5 = 35
+    WEDNESDAY6 = 36
+    WEDNESDAY7 = 37
+    WEDNESDAY8 = 38
+    WEDNESDAY9 = 39
+    THURSDAY1 = 41
+    THURSDAY2 = 42
+    THURSDAY3 = 43
+    THURSDAY4 = 44
+    THURSDAY5 = 45
+    THURSDAY6 = 46
+    THURSDAY7 = 47
+    THURSDAY8 = 48
+    THURSDAY9 = 49
+    FRIDAY1 = 51
+    FRIDAY2 = 52
+    FRIDAY3 = 53
+    FRIDAY4 = 54
+    FRIDAY5 = 55
+    FRIDAY6 = 56
+    FRIDAY7 = 57
+    FRIDAY8 = 58
+    FRIDAY9 = 59
+  # Fields of this model
+  lesson = models.OneToOneField(
+    Lesson,
+    on_delete=models.CASCADE,
+    related_name='valid_status',
+    null=True,
+    blank=True
+  )
+  teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+  begin_date = models.DateField()
+  begin_semester = models.CharField(choices=SEMESTER)
+  begin_time = models.IntegerField(choices=BEGIN_TIME_TYPE)
+
+class LessonSelect(models.Model):
+  valid_lesson = models.ForeignKey(ValidLesson, on_delete=models.CASCADE)
+  student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  select_date = valid_lesson.begin_date
+  select_semester = valid_lesson.begin_semester
+  score = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
+

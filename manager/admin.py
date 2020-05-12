8dd61cac67cbea5478c3d0acf741a2157b3c837d
ShadowTinker <shadowtinkery@gmail.com>
2020-05-12 16:26:32 +0800

    Fix some bugs and change settings: 1. Bug fix:     1.1 fix tuple error by adding a comma     1.2 change related_name to apply 2 different StatusChange     1.3 change name of Class_ to myClass and fix correspondent sentences     1.4 change ManyToMany relationship from Student-Lesson to Student-ValidLesson 2. Settings change:     2.1 add urls for the app MANAGER     2.2 register some models, which I think is necessary, to admin.site     2.3 make migrations and implement migrating     2.4 update .gitignore

--- manager/admin.py
+++ manager/admin.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Campus)
+admin.site.register(models.Major)
+admin.site.register(models.myClass)
+admin.site.register(models.Teacher)
+admin.site.register(models.Student)
+admin.site.register(models.GradeTransfer)
+admin.site.register(models.MajorTransfer)
+admin.site.register(models.ValidLesson)
+admin.site.register(models.LessonSelect)

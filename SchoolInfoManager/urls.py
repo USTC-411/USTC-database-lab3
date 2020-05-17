20d759d7a28ece36c2d55f6d645de87f17ce12f3
shadowtinkery <shadowtinkery@gmail.com>
2020-05-17 11:20:58 +0800

    1. change urls of main app to locate index.html in manager 2. change views of the app 'manager' to render a request to index.html 3. add a file index.html in templates of the app 'manager', in which 	3.1 add a search bar 	3.2 add 9 cards for different information management 	3.3 all the elements are implemented by using bootstrap

--- SchoolInfoManager/urls.py
+++ SchoolInfoManager/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manager/', include('manager.urls')),
+    path('', views.index),
 ]

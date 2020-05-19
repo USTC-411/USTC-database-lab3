00cc290516af95995b247c76625c8241c5d2b53c
shadowtinkery <shadowtinkery@gmail.com>
2020-05-19 14:44:13 +0800

    Add templates 1. add 9 templates for query 2. add IndexForTeacher 3. change the name of index.html to IndexForStudent.html 4. change urls to apply the new name of Students' query page

--- manager/views.py
+++ manager/views.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def index(request):
-  return render(request, 'manager/index.html')
+def IndexForStudent(request):
+  return render(request, 'manager/IndexForStudent.html')

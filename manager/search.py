ba543ae90b7e88b5341ff9663d289b734ab234ab
Liwuchao <liwuchao@mail.ustc.edu.cn>
2020-05-21 16:06:32 +0800

    finish course table search

--- manager/search.py
+++ manager/search.py
@@ -17,12 +17,11 @@ def global_search(request):
     global_info = request.GET.get('global_info').strip()
   if global_info:
     stu_list = Student.objects.filter(Q(name__contains=global_info) | Q(student_id__contains=global_info)| Q(myClass__name__contains=global_info))
-    cou_list = ValidLesson.objects.filter(Q(lesson__name__contains=global_info) | Q(teacher__name__contains=global_info) | Q(lesson__id__contains=global_info) | Q(lesson__test_type__contains=global_info))
+    cou_list = ValidLesson.objects.filter(Q(lesson__name__contains=global_info) | Q(teacher__name__contains=global_info) | Q(lesson__id__contains=global_info) | Q(begin_time__contains=global_info) | Q(begin_semester__contains=global_info))
   return render(request,'manager/search.html',{'stu_list':stu_list,'cou_list':cou_list})
 
 def stu_search(request):
   stu_list = []
-  stu={}
   if request.method == "GET":
     StudentID = request.GET.get('StudentID').strip()
     StudentName = request.GET.get('StudentName').strip()
@@ -34,5 +33,12 @@ def stu_search(request):
 
 def cou_search(request):
   cou_list=[]
-  cou={}
+  if request.method == "GET":
+    CourseID = request.GET.get('CourseID').strip()
+    CourseName = request.GET.get('CourseName').strip()
+    CourseTeacher = request.GET.get('CourseTeacher').strip()
+    CourseTime = request.GET.get('CourseTime').strip()
+    CourseSemester = request.GET.get('CourseSemester').strip()
+  if CourseID or CourseName or CourseTeacher or CourseTime or CourseSemester:
+    cou_list = ValidLesson.objects.filter(Q(lesson__id__contains=CourseID) & Q(lesson__name__contains=CourseName) & Q(teacher__name__contains=CourseTeacher) & Q(begin_time__contains=CourseTime) & Q(begin_semester__contains=CourseSemester))
   return render(request,'manager/search.html',{'cou_list':cou_list})

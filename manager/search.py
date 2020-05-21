3c41a8aa1c8d1d446b3572621c72a8382c24f7b4
Liwuchao <liwuchao@mail.ustc.edu.cn>
2020-05-21 16:22:13 +0800

    Add nationality option in stu_search

--- manager/search.py
+++ manager/search.py
@@ -16,7 +16,7 @@ def global_search(request):
   if request.method == "GET":
     global_info = request.GET.get('global_info').strip()
   if global_info:
-    stu_list = Student.objects.filter(Q(name__contains=global_info) | Q(student_id__contains=global_info)| Q(myClass__name__contains=global_info))
+    stu_list = Student.objects.filter(Q(name__contains=global_info) | Q(student_id__contains=global_info)| Q(myClass__name__contains=global_info) | Q(nationality__contains=global_info))
     cou_list = ValidLesson.objects.filter(Q(lesson__name__contains=global_info) | Q(teacher__name__contains=global_info) | Q(lesson__id__contains=global_info) | Q(begin_time__contains=global_info) | Q(begin_semester__contains=global_info))
   return render(request,'manager/search.html',{'stu_list':stu_list,'cou_list':cou_list})
 
@@ -27,8 +27,9 @@ def stu_search(request):
     StudentName = request.GET.get('StudentName').strip()
     StudentClass = request.GET.get('StudentClass').strip()
     StudentSex = request.GET.get('StudentSex').strip()
-  if StudentID or StudentName or StudentClass or StudentSex:
-    stu_list = Student.objects.filter(Q(name__contains=StudentName) & Q(student_id__contains=StudentID) & Q(myClass__name__contains=StudentClass) & Q(sex__contains=StudentSex))
+    StudentNation = request.GET.get('StudentNation').strip()
+  if StudentID or StudentName or StudentClass or StudentSex or StudentNation:
+    stu_list = Student.objects.filter(Q(name__contains=StudentName) & Q(student_id__contains=StudentID) & Q(myClass__name__contains=StudentClass) & Q(sex__contains=StudentSex) & Q(nationality__contains=StudentNation))
   return render(request,'manager/search.html',{'stu_list':stu_list})
 
 def cou_search(request):
